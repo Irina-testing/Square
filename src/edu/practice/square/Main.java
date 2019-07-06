2515818b09ce8d8cfe48aa8c3cf43fb26580da59
Irina <Irinkastar@mail.ru>
2019-07-06 11:27:41 +0300

    6.07.2019

--- src/edu/practice/square/Main.java
+++ src/edu/practice/square/Main.java
@@ -20,8 +20,7 @@ public class Main {
             if (x1 == x2) {
                 System.out.println(x1);
             } else {
-                System.out.println(x1);
-                System.out.println(x2);
+                System.out.println(x1 + ";" + x2);
             }
         }
     }
